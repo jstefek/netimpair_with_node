bdc90bd8de71367d4bdcbd07325be8b485f08052
Jiri Stefek <jstefek01@gmail.com>
2018-06-17 11:18:37 +0200

    downlink excluded

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -4,13 +4,23 @@ var exec = require('child_process').exec,
     active = false,
     PORT_HTTPS = process.env.PORT_HTTPS || 443,
     PORT_HTTP = process.env.PORT_HTTP || 80,
+    PORT_VNC = process.env.PORT_VNC || 5900,
+    PORT_SELENIUM_HUB = process.env.HUB_PORT_4444_TCP_PORT || 4444,
+    PORT_SELENIUM_NODE = process.env.NODE_PORT || 5555,
+    IP_REGEX = /^\d+\.\d+\.\d+\.\d+$/,
+    PORT_REGEX = /^\d+$/,
+    SPEC_REGEX = /^(src|dst|sport|dport)=/,
     netimpairProcess;
 
+/**
+ * Returns an array, either empty or with parsed values
+ */
 function parseArray(str) {
     if (!str) {
         result = [];
     } else {
         var result = str.split(',').forEach(function (value) {
+            // trim
             return value.replace(/^\s+|\s+$/g, '');
         });
         result = !!result ? result : [];
@@ -19,35 +29,60 @@ function parseArray(str) {
 }
 
 function buildCommandParams(query) {
-    var isDownlink = query.direction == 'downlink';
-    var direction = isDownlink ? 'src' : 'dst';
+    // var isDownlink = query.direction === 'downlink';
     var excluded = parseArray(query.excluded);
+
+    excluded.push(PORT_SELENIUM_HUB);
+    excluded.push(PORT_SELENIUM_NODE);
+    excluded.push(PORT_HTTP);
+    excluded.push(PORT_HTTPS);
+    excluded.push(PORT_VNC);
+
     var included = parseArray(query.included);
     var type = query.type;
 
     var command = [];
     command.push('-n ' + query.networkInterface);
 
-    if (!included || included.length == 0) {
+    if (included.length === 0) {
         // exclude all given addresses
         excluded.forEach(function (val) {
-            command.push('--exclude src=' + val);
-            command.push('--exclude dst=' + val);
+            if (val.match(SPEC_REGEX)) {
+                command.push('--exclude ' + val);
+            } else {
+                if (val.match(IP_REGEX)) {
+                    command.push('--exclude src=' + val);
+                    command.push('--exclude dst=' + val);
+                } else if (val.match(PORT_REGEX)) {
+                    command.push('--exclude sport=' + val);
+                    command.push('--exclude dport=' + val);
+                }
+                else {
+                    throw 'UNKNOWN value to exclude: <' + val + '>';
+                }
+            }
         });
-        command.push('--exclude dport=' + PORT_HTTPS);
-        command.push('--exclude sport=' + PORT_HTTPS);
-        command.push('--exclude dport=' + PORT_HTTP);
-        command.push('--exclude sport=' + PORT_HTTP);
-        command.push('--exclude dport=4444');
-        command.push('--exclude sport=4444');
     }
     included.forEach(function (val) {
-        command.push('--include ' + direction + '=' + val);
+        if (val.match(SPEC_REGEX)) {
+            command.push('--include ' + val);
+        } else {
+            if (val.match(IP_REGEX)) {
+                command.push('--include src=' + val);
+                command.push('--include dst=' + val);
+            } else if (val.match(PORT_REGEX)) {
+                command.push('--include sport=' + val);
+                command.push('--include dport=' + val);
+            }
+            else {
+                throw 'UNKNOWN value to exclude: <' + val + '>';
+            }
+        }
     });
-    if (isDownlink) {
-        command.push('--inbound');
-    }
-    command.push((type == 'limit' ? 'rate ' : 'netem') + ' --' + type + ' ' + query.value)
+    // if (isDownlink) {
+    //     command.push('--inbound');
+    // }
+    command.push((type === 'limit' ? 'rate ' : 'netem') + ' --' + type + ' ' + query.value);
     command.push('--toggle ' + query.duration);
     return command.join(' ');
 }
